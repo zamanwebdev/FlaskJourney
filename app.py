711b8301b076c80825f6f2d2a38ba30e84f264c2
Syed Zaman <zamanwebdev@gmail.com>
2026-02-16 22:12:47 +0600

    Day 2- Dynamic Routing + Jinja2 Mastery Done.

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("home.html", name="Zaman")
+
+@app.route('/profile/<name>')
+def profile(name):
+    skills = ["Python", "Flask", "WordPress", "Automation"]
+    return render_template("profile.html", username=name, skills=skills)
 
 if __name__ == '__main__':
     app.run(debug=True)
